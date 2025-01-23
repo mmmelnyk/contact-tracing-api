3f9969488bbbedfaa4259c458afa1c0b5238e16f
Melnyk Mykola <kolanio757@gmail.com>
2025-01-23 12:20:07 +0200

    Add more tests

--- ContactTracingAPI.Tests/UserTests.cs
+++ ContactTracingAPI.Tests/UserTests.cs
@@ -32,4 +32,63 @@ public class UserTests
         // Assert
         userRepoMoq.Verify(x => x.InsertAsync(It.IsAny<User>(), It.IsAny<CancellationToken>()));
     }
+
+    [Test]
+    public async Task DeleteUserRequest_UserDeletedInDbAsync()
+    {
+        // Arrange
+        var mediator = new  Mock<IMediator>();
+        var userRepoMoq = new Mock<IUserRepository>();
+        var request = new DeleteUserRequest
+        {
+            Id = new Guid(),
+        };
+        var handler = new DeleteUserRequestHandler(userRepoMoq.Object);
+
+        // Act
+        var response = await handler.Handle(request, new CancellationToken());
+        
+        // Assert
+        userRepoMoq.Verify(x => x.RemoveAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()));
+    }
+
+    [Test]
+    public async Task UpdateUserRequest_UserUpdatedInDbAsync()
+    {
+        // Arrange
+        var mediator = new  Mock<IMediator>();
+        var userRepoMoq = new Mock<IUserRepository>();
+        var request = new UpdateUserRequest
+        {
+            Id = new Guid(),
+            DeviceId = "someDeviceId",
+            LastLogin = DateTime.UtcNow.ToString("o")
+        };
+        var handler = new UpdateUserRequestHandler(userRepoMoq.Object);
+
+        // Act
+        var response = await handler.Handle(request, new CancellationToken());
+        
+        // Assert
+        userRepoMoq.Verify(x => x.UpdateAsync(It.IsAny<User>(), It.IsAny<CancellationToken>()));
+    }
+
+    [Test]
+    public async Task GetUserRequest_UserReturned_FromDbAsync()
+    {
+        // Arrange
+        var mediator = new  Mock<IMediator>();
+        var userRepoMoq = new Mock<IUserRepository>();
+        var request = new GetUserRequest
+        {
+            Id = new Guid()
+        };
+        var handler = new GetUserRequestHandler(userRepoMoq.Object);
+
+        // Act
+        var response = await handler.Handle(request, new CancellationToken());
+        
+        // Assert
+        userRepoMoq.Verify(x => x.GetByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()));
+    }
 }
